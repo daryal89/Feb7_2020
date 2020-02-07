cc5a05fa7e55629e9632dd3037dd69b54d89e7f0
daryal89 <creativity80@gmail.com>
2020-02-07 09:48:08 -0500

    some code added

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,13 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void login() {
+		System.out.println("Logged into the system!!!");
+	}
+	
+	public void logout() {
+		System.out.println("logout form the system !!!");
+	}
 
 }
--- src/test/java/com/qa/tests/LoginPageTest.java
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -1,5 +1,12 @@
 package com.qa.tests;
 
+import org.testng.annotations.Test;
+
 public class LoginPageTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Test successful");
+	}
 
 }
