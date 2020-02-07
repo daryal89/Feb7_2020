f2f8d75b0bd4af77df72beef0f4d33f9bb9bc526
daryal89 <creativity80@gmail.com>
2020-02-07 10:15:45 -0500

    propeties file added

--- src/main/java/com/qa/pages/ContactsPage.java
+++ src/main/java/com/qa/pages/ContactsPage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class ContactsPage {
+	
+	
+	public void createContact() {
+		System.out.println("Contact created!!!");
+	}
+	
+	public void updateContact() {
+		System.out.println("Contact Updated!!!");
+	}
 
 }
