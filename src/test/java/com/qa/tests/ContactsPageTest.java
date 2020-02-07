7e9eb60e077e096f14ad23f13ee2f85a385b3865
daryal89 <creativity80@gmail.com>
2020-02-07 09:51:51 -0500

    added some code in classes

--- src/test/java/com/qa/tests/ContactsPageTest.java
+++ src/test/java/com/qa/tests/ContactsPageTest.java
@@ -1,5 +1,12 @@
 package com.qa.tests;
 
+import org.testng.annotations.Test;
+
 public class ContactsPageTest {
+	
+	@Test
+	public void createContactDetail() {
+		System.out.println("Contactdetails created");
+	}
 
 }
